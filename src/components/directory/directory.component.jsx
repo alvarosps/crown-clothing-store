38377b13890bfdbc77d1ea9bea9f0ed16b1485ea
Alvaro Silva <alvaro123sps@gmail.com>
2021-01-08 17:59:17 -0300

    implemented firebase utils, included ability to store authenticated users into the firestorm db

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -46,7 +46,7 @@ class Directory extends React.Component {
             <div className='directory-menu'>
                 {
                     this.state.sections.map(({ title, imageUrl, id, size }) => (
-                        <MenuItem id={id} title={title} imageUrl={imageUrl} size={size} />
+                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
                     ))
                 }
             </div>
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -4,16 +4,41 @@ import 'firebase/auth';
 
 
 const config = {
-    apiKey: "AIzaSyB9dMwcOLNEh4r_3i5aYkP4oC9sFnxOcbA",
-    authDomain: "crown-db-46bd8.firebaseapp.com",
-    projectId: "crown-db-46bd8",
-    storageBucket: "crown-db-46bd8.appspot.com",
-    messagingSenderId: "982277250013",
-    appId: "1:982277250013:web:a974a05fda30e944b93434",
-    measurementId: "G-Y3MYVRGRFQ"
-  };
+    apiKey: "AIzaSyCfg2Q99ADH5d_o1pgzgAyok0R4FV2o3_4",
+    authDomain: "sps-db-d4d40.firebaseapp.com",
+    projectId: "sps-db-d4d40",
+    storageBucket: "sps-db-d4d40.appspot.com",
+    messagingSenderId: "830392778813",
+    appId: "1:830392778813:web:8b9429c8faa57af3a9ea54"
+};
 
-firebase.initializeApp(config);
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+	if (!userAuth) return;
+
+	const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+	const snapShot = await userRef.get();
+
+	if (!snapShot.exists) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createdAt,
+				...additionalData
+			});
+		} catch (e) {
+			console.log('error creating user', e);
+		}
+	}
+
+	return userRef;
+}
+
+!firebase.apps.length ? firebase.initializeApp(config) : firebase.app();
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
